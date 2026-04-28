387e93dda497530901c35dbf7297c3e061813333
Emmanuel Bosquet <keksoj@proton.me>
2026-04-28 21:15:23 +0200

    GameError::Flush and GameError::Write

--- src/game.rs
+++ src/game.rs
@@ -19,6 +19,12 @@ pub enum GameError {
     TerminalSize(IoError),
     #[error("could not switch terminal to raw mode: {0}")]
     TerminalRawMode(IoError),
+    #[error("IO read error: {0}")]
+    IoRead(IoError),
+    #[error("Flush error: {0}")]
+    Flush(IoError),
+    #[error("Write error: {0}")]
+    Write(IoError),
 }
 pub struct Game<R, W: Write> {
     width: usize,
@@ -57,26 +63,26 @@ impl<R: Read, W: Write> Game<R, W> {
         Ok(new_game)
     }
 
-    pub fn run(&mut self) {
+    pub fn run(&mut self) -> Result<(), GameError> {
         loop {
-            self.take_direction();
+            self.take_direction()?;
             self.snake.advance(self.width);
             self.check_for_food();
             self.check_for_collisions();
             self.write_snake_and_food_on_the_board();
-            self.display_board();
+            self.display_board()?;
             thread::sleep(time::Duration::from_millis(self.tick_time));
         }
     }
 
-    pub fn take_direction(&mut self) {
+    pub fn take_direction(&mut self) -> Result<(), GameError> {
         match self.snake.direction {
             Direction::Up | Direction::Down => self.snake.turning = Turning::Keepvertical,
             Direction::Left | Direction::Right => self.snake.turning = Turning::Keephorizontal,
         }
 
         let mut b = [0];
-        self.stdin.read(&mut b).unwrap();
+        self.stdin.read(&mut b).map_err(GameError::IoRead)?;
         match b[0] {
             b'i' => self.snake.take_direction(Direction::Up),
             b'j' => self.snake.take_direction(Direction::Left),
@@ -85,7 +91,8 @@ impl<R: Read, W: Write> Game<R, W> {
             b'q' => panic!("c'est la panique !"),
             _ => {}
         }
-        self.stdout.flush().unwrap();
+        self.stdout.flush().map_err(GameError::Flush)?;
+        Ok(())
     }
 
     fn check_for_collisions(&mut self) {
@@ -119,49 +126,70 @@ impl<R: Read, W: Write> Game<R, W> {
         }
     }
 
-    pub fn display_board(&mut self) {
-        write!(self.stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+    pub fn display_board(&mut self) -> Result<(), GameError> {
+        write!(self.stdout, "{}{}", clear::All, cursor::Goto(1, 1)).map_err(GameError::Write)?;
 
         // the top wall
-        self.stdout.write(cell::TOP_LEFT_CORNER.as_bytes()).unwrap();
+        self.stdout
+            .write(cell::TOP_LEFT_CORNER.as_bytes())
+            .map_err(GameError::Write)?;
+
         for _n in 0..self.width {
-            self.stdout.write(cell::CEILING.as_bytes()).unwrap();
+            self.stdout
+                .write(cell::CEILING.as_bytes())
+                .map_err(GameError::Write)?;
         }
         self.stdout
             .write(cell::TOP_RIGHT_CORNER.as_bytes())
-            .unwrap();
-        self.stdout.write(b"\n\r").unwrap();
+            .map_err(GameError::Write)?;
+
+        self.stdout.write(b"\n\r").map_err(GameError::Write)?;
 
         // display each line
         for line in self.board.as_slice().chunks(self.width) {
-            self.stdout.write(cell::WALL.as_bytes()).unwrap();
+            self.stdout
+                .write(cell::WALL.as_bytes())
+                .map_err(GameError::Write)?;
+
             for &cell in line {
                 self.stdout
                     .write(cell.match_to_symbol().as_bytes())
-                    .unwrap();
+                    .map_err(GameError::Write)?;
             }
-            self.stdout.write(cell::WALL.as_bytes()).unwrap();
-            self.stdout.write(b"\n\r").unwrap();
+            self.stdout
+                .write(cell::WALL.as_bytes())
+                .map_err(GameError::Write)?;
+            self.stdout.write(b"\n\r").map_err(GameError::Write)?;
         }
 
         // the bottom wall
         self.stdout
             .write(cell::BOTTOM_LEFT_CORNER.as_bytes())
-            .unwrap();
+            .map_err(GameError::Write)?;
         for _n in 0..self.width {
-            self.stdout.write(cell::CEILING.as_bytes()).unwrap();
+            self.stdout
+                .write(cell::CEILING.as_bytes())
+                .map_err(GameError::Write)?;
         }
         self.stdout
             .write(cell::BOTTOM_RIGHT_CORNER.as_bytes())
-            .unwrap();
-        self.stdout.write(b"\n\r").unwrap();
+            .map_err(GameError::Write)?;
+        self.stdout.write(b"\n\r").map_err(GameError::Write)?;
 
         // score
-        self.stdout.write(b"\n\rYour score: ").unwrap();
+        self.stdout
+            .write(b"\n\rYour score: ")
+            .map_err(GameError::Write)?;
+
         let snake_length_string: String = self.snake.body.len().to_string();
         let snake_position_bytes: &[u8] = snake_length_string.as_bytes();
-        self.stdout.write(snake_position_bytes).unwrap();
-        self.stdout.flush().unwrap();
+        self.stdout
+            .write(snake_position_bytes)
+            .map_err(GameError::Write)?;
+
+        self.stdout.flush().map_err(GameError::Flush)?;
+
+        Ok(())
     }
 
     fn game_over(&mut self, message: String) {
